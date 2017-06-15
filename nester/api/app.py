50d94b46a3a42a8cab20eeb765e3f566ab6f3063
8891 <test1@nest.yt>
2017-06-15 07:05:06 +0000

    direct url used in initial pull to avoid connetion fails

--- nester/api/app.py
+++ nester/api/app.py
@@ -60,13 +60,14 @@ class App(Cloud):
     def attach(self):
         self.remove_owner()
         self.create_owner()
-
+	host = os.environ['NEST_CONTACT_ID'] + '@' + os.environ['NEST_APP_TAG']+".nestapp.yt"
+ 
         if (os.environ['NEST_PLATFORM_TAG'] != 'worker'):
-            self.os_exec("/usr/bin/rsync -avzrh --timeout=30 --progress nest:/var/app/app.nest /var/app")
-            self.os_exec("/usr/bin/rsync -avzrh --timeout=30 --progress nest:/var/app/app.json /var/app")
-            self.os_exec("/usr/bin/rsync -avzrh --timeout=30 --progress nest:/var/app/nest /var/app")
-            self.os_exec("/usr/bin/rsync -avzrh --timeout=30 --progress nest:/var/app/log /var/app")
-        self.os_exec("/usr/bin/rsync -avzrh --timeout=30 --progress nest:" + os.environ['NEST_FOLDER_SOURCE'] + " /var/app/source")
+            self.os_exec("/usr/bin/rsync -avzrh --timeout=30 --progress "+ host +":/var/app/app.nest /var/app")
+            self.os_exec("/usr/bin/rsync -avzrh --timeout=30 --progress "+ host +":/var/app/app.json /var/app")
+            self.os_exec("/usr/bin/rsync -avzrh --timeout=30 --progress "+ host +":/var/app/nest /var/app")
+            self.os_exec("/usr/bin/rsync -avzrh --timeout=30 --progress "+ host +":/var/app/log /var/app")
+        self.os_exec("/usr/bin/rsync -avzrh --timeout=30 --progress "+ host +":" + os.environ['NEST_FOLDER_SOURCE'] + " /var/app/source")
 
 	self.os_exec("mkdir -p " + os.environ['NEST_FOLDER_PUBLISH'])
         self.os_exec("/bin/bash " + self.get_twig_utils_folder() + "/create")
@@ -107,7 +108,7 @@ class App(Cloud):
         self.os_exec("echo "+os.environ['NEST_CONTACT_KEY']+" | base64 --decode > /home/"+os.environ['NEST_CONTACT_ID']+"/.ssh/id_rsa")
         self.os_exec("chmod -R 600 /home/"+os.environ['NEST_CONTACT_ID']+"/.ssh/*")
         self.os_exec("ssh-keygen -f /home/"+os.environ['NEST_CONTACT_ID']+"/.ssh/id_rsa -y > /home/"+os.environ['NEST_CONTACT_ID']+"/.ssh/id_rsa.pub")
-        self.os_exec("ssh-keyscan -t rsa "+os.environ['NEST_APP_TAG']+".nestapp.yt > /home/"+os.environ['NEST_CONTACT_ID']+"/.ssh/known_hosts")
+        #self.os_exec("ssh-keyscan -t rsa "+os.environ['NEST_APP_TAG']+".nestapp.yt > /home/"+os.environ['NEST_CONTACT_ID']+"/.ssh/known_hosts")
         self.os_exec("printf 'Host nest\n\tHostName "+os.environ['NEST_APP_TAG']+".nestapp.yt\n\tUserKnownHostsFile /dev/null\n\tStrictHostKeyChecking no\n\tUser "+os.environ['NEST_CONTACT_ID']+"\n' > /home/"+os.environ['NEST_CONTACT_ID']+"/.ssh/config")
         self.os_exec("chown -R "+os.environ['NEST_CONTACT_ID']+":tree /home/"+os.environ['NEST_CONTACT_ID']+"/.ssh")
         self.os_exec("chmod 700 /home/"+os.environ['NEST_CONTACT_ID']+"/.ssh")
